050679cca4a011f6144f37ada96236c30472a2a6
jsvanilla <36719905+jsvanilla@users.noreply.github.com>
2023-01-21 23:43:01 -0600

    CHANGES turn off smartpy class temporary  because sp.Contract only exist in smartpy IDE

--- AR-FIELTS/tezos/main.py
+++ AR-FIELTS/tezos/main.py
@@ -1,14 +1,31 @@
 import smartpy as sp
 import pandas as pd
 
+# print("sp")
+# print(sp)
 
-class Ar_Fielts(sp.Contract):
 
-    def __init__(self, data):
-        self.init(data=data)
+class Ar_Fielts:
+    def __init__(self, data, skill):
+        self.data = data
+        self.skill = skill
 
-    @sp.entry_point
-    def average(self, params):
-        df = pd.DataFrame(self.data.data)
-        avg = df[params.column].mean()
-        return avg
+    def average(self):
+        print("mi data")
+        df = pd.read_csv('../fake_oscar_dataset.csv', sep=';')
+        df.sort_values("timestamp", ascending=True, inplace=True)
+        my_query = f"topics.str.contains('{self.skill}')"
+        print(my_query)
+        print(df.query("topics.str.contains('react')"))
+
+        # class Ar_Fielts(sp.Contract):
+
+        #     def __init__(self, data):
+        #         self.init(data=data)
+
+        #     @sp.entry_point
+        #     def average(self, params):
+        #         # df = pd.DataFrame(self.data.data)
+        #         print(self.data)
+        #         # avg = df[params.column].mean()
+        #         # return avg
--- AR-FIELTS/tezos/use_example.py
+++ AR-FIELTS/tezos/use_example.py
@@ -1,11 +1,10 @@
 from main import Ar_Fielts
-import pandas as pd
-import csv
 
-with open('ruta/al/archivo.csv', 'r') as file:
-    fake_data = csv.reader(file)
+# The contracts receives the uuid key for extract and decrypt the data of another protocol, and the skill to measure
+contract = Ar_Fielts(fake_data, 'react')
 
-contract = Ar_Fielts(data=fake_data)
-result = contract.average(column="tags").export()
+result = contract.average()
 
-assert result == 3.0
+# result = contract.average(column="topics").export()
+
+# assert result == 3.0
